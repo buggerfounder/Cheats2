d22f5c2860b623f71f9d5a0784d9499b6ccf7512
buggerfounder <144815577+buggerfounder@users.noreply.github.com>
2025-11-08 18:15:34 +0300

    Create maincheatSB.lua

---
+++ maincheatSB.lua
@@ -0,0 +1,27 @@
+local partiros = Instance.new("Part", workspace)
+partiros.Size = Vector3.new(100000, 1, 100000)
+local UIS = game:GetService("UserInputService")
+print("Hi this is working Script Cobaken555")
+local debounce = false
+
+local function notification(Text, Subtext, Time)
+	if debounce == false then 
+		debounce = true
+		game.StarterGui:SetCore("SendNotification", {
+			Title = Subtext;
+			Text = Text;
+			Duration = Time;})
+		task.wait(1)
+		debounce = false
+	end
+end
+notification("Hi This script Working", "WORKS", 3)
+
+UIS.InputBegan:Connect(function(plr)
+	if plr.KeyCode == Enum.KeyCode.E then
+		partiros.Position = partiros.Position + Vector3.new(0,1,0)
+	end
+	if plr.KeyCode == Enum.KeyCode.Q then
+		partiros.Position = partiros.Position - Vector3.new(0,1,0)
+	end
+end)
